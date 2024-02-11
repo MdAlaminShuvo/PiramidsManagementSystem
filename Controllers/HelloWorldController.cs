53eeea01bb4fa4e9f343d62fd8dacfc452122ea4
Md Al Amin <alamin.cse.ru2018@gmail.com>
2024-02-11 13:48:38 +0600

    Piramids Managements system are created

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PiramidsManagementSystem.Models;
+
+namespace PiramidsManagementSystem.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        private static List<DogViewModel> dogList = new List<DogViewModel>();
+        public IActionResult Index()
+        {
+            DogViewModel doggo = new DogViewModel()
+            {
+                Name = "Sif",
+                Age = 2
+            };
+
+            dogList.Add(doggo);
+
+            return View(dogList);
+        }
+        public IActionResult Create()
+        {
+            var dogVm = new DogViewModel();
+            return View(dogVm);
+        }
+        public IActionResult CreateDog(DogViewModel dogViewModel)
+        {
+            dogList.Add(dogViewModel);
+            return RedirectToAction("Index");
+        }
+        public string Hello()
+        {
+            return "Who's there?";
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PiramidsManagementSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
